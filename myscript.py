d4f8667d4dd275854321d411e79073b2d11571b1
yassinerocmaaa <yassine.el-asri@polymtl.ca>
2023-03-24 18:45:53 -0400

    Editeded

--- myscript.py
+++ myscript.py
@@ -1,7 +1,8 @@
 import os
 
+# Commits bon et mauvais
+badhash = 'c1a4be04b972b6c17db242fc37752ad517c29402'
+goodhash = 'e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c'
 
-os.system('git bisect start')
-os.system('git bisect good c1a4be04b972b6c17db242fc37752ad517c29402')
-os.system('git bisect bad e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c')
+os.system(f'git bisect start {badhash} {goodhash}')
 os.system('git bisect run python manage.py test')
